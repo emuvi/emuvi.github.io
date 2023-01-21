805e050356afc657ca8b2492d2fad4c5649b5ae2
Éverton M. Vieira <emuvi@outlook.com.br>
2023-01-21 18:44:07 -0300

    new format with all actual degrees

--- index.js
+++
@@ -1,6 +0,0 @@
-function readMore(elementId, fromId) {
-  var element = document.getElementById(elementId);
-  element.style.display = "initial";
-  var from = document.getElementById(fromId);
-  from.style.display = "none";
-}
