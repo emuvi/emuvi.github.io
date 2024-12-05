0108a3ecfba3536ca6715535d5add9a7ef2a709f
Éverton M. Vieira <emuvi@outlook.com.br>
2024-12-05 13:39:00 -0300

    new year updates

--- update.py
+++ update.py
@@ -1,4 +1,5 @@
 import os
+import urllib.parse
 
 end_tag = "<!--end-->"
 
@@ -10,24 +11,32 @@ with open('index.html', mode="r", encoding="utf-8") as index:
 def make_degrees():
     global source  
     for category in os.listdir("degrees"):
-        begin_tag = "<!--begin degrees/" + category + "-->"
-        start = source.index(begin_tag)
-        if start < 0:
-            raise "Could not found where to put the degrees of: " + category;
-        end = source.index(end_tag, start + len(begin_tag))
-        if end < 0:
-            raise "Could not found where to end the degrees of: " + category;
-        made = "";
-        for degree in sorted(os.listdir("degrees/" + category), reverse=True):
-            made += "\n              <div><a href='link'>" + degree + "</a></div>";
+        begin_tag = f"<!--begin degrees/{category}-->"
+        try:
+            start = source.index(begin_tag)
+        except ValueError:
+            raise ValueError(f"Could not find where to put the degrees of: {category}")
+        
+        try:
+            end = source.index(end_tag, start + len(begin_tag))
+        except ValueError:
+            raise ValueError(f"Could not find where to end the degrees of: {category}")
+        
+        made = ""
+        for degree in sorted(os.listdir(f"degrees/{category}"), reverse=True):
+            encoded_name = urllib.parse.quote(degree)  # Codifica o nome do arquivo
+            link_path = f"degrees/{category}/{encoded_name}"  # Constrói o caminho do link
+            made += f"\n              <div><a href='{link_path}'>{degree}</a></div>"
         made += "\n              "
-        source = source[:start+len(begin_tag)] + made + source[end:]
+        source = source[:start + len(begin_tag)] + made + source[end:]
 
 
 def save():
     with open('index_new.html', mode="w", encoding="utf-8") as index_new:
         index_new.write(source)
 
+
 if __name__ == '__main__':
     make_degrees()
     save()
+    print("Done!")
